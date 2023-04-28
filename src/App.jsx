a3cae9b52f202c599046f6323bfed91b5a08b297
abkux <kumarjhaabhishek2002@gmail.com>
2023-04-28 20:30:53 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ export default function App() {
       <span className="text-center font-bold text-5xl text-red-500">Question Papers</span>
       <span className="mt-10 text-xs text-gray-500">⚠️Note: You can submit question paper by using below link!</span>
       <p className="text-xs text-gray-500">🔍 Press <span className="font-bold text-black">(Ctrl + F)</span> and enter the subject code you looking.</p>
-      <button className="mt-2 text-base bg-red-500 hover:bg-red-400 text-white p-2 rounded-lg drop-shadow-lg" onClick={() => (window.location.href = "https://forms.gle/ckkPitjtGrA5yXrp7")}>→ Submit Paper</button>
-    </div>
+      <div className="flex-row text-center">
+      <button className="mt-2 text-base bg-red-500 hover:bg-red-400 text-white p-2 rounded-lg drop-shadow-lg" onClick={() => (window.location.href = "https://forms.gle/ckkPitjtGrA5yXrp7")}>Submit Paper (Mid Term)</button>
+      <button className="mt-2 ml-0 sm:ml-4 text-base bg-red-500 hover:bg-red-400 text-white p-2 rounded-lg drop-shadow-lg" onClick={() => (window.location.href = "https://forms.gle/ckkPitjtGrA5yXrp7")}>Submit Paper (Term End)</button>
+        </div>
+        </div>
       <div className="mt-20 flex flex-col items-center">
         <>
         <h className="font-bold text-2xl sm:text-3xl">CHY1006</h>
